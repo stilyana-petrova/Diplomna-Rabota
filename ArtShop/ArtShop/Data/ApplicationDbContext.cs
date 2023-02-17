fb4048b60a7fb2d69c5dd0ecfedcd0a25ad43f16
stilyana-petrova <stilyanapetrova1@gmail.com>
2023-02-17 08:07:29 +0200

    new commit

--- ArtShop/ArtShop/Data/ApplicationDbContext.cs
+++ ArtShop/ArtShop/Data/ApplicationDbContext.cs
@@ -7,7 +7,7 @@ using System.Text;
 
 namespace ArtShop.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
